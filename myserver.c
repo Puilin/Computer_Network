e1c11cb75c02936162520cf8418bd20af21bd6c0
puilin <suryblue@naver.com>
2022-04-10 01:25:48 +0900

    add : print request message from client

--- myserver.c
+++ myserver.c
@@ -5,11 +5,17 @@
 #include <string.h> // for strlen
 #include <netinet/in.h> // for struct sockaddr_in
 #include <arpa/inet.h> // for inet_addr
+#include <fcntl.h> // for O_RDONLY
 
 #define MAX_LINE 20
+#define REQ_LINE 200
 
 // funtion prototype
 int take_input();
+void handle_request(int client_sd);
+char *get_content_type(char *filename);
+void send_data(FILE *client_w, char *ct, char *file_name);
+void send_error(FILE *client_w);
 
 int main()
 {
@@ -19,6 +25,7 @@ int main()
     struct sockaddr_in client_addr; // socket address structure (client)
     socklen_t client_size;
     char args[2][12]; // placeholder for command line
+    pthread_t tid;
 
     port = take_input(args);
     printf("set port : %d\n", port);
@@ -43,11 +50,11 @@ int main()
     /* for reusing address and port
     because the func bind() binds the address and port only once */
     int opt = 1;
-    if (setsockopt(server_sd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_sd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("set sockopt(address)");
         return 1;
     }
-    if (setsockopt(server_sd, SOL_SOCKET, SO_REUSEPORT, (char *)&opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_sd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
         perror("set sockopt(port)");
         return 1;
     }
@@ -65,20 +72,18 @@ int main()
     }
 
     // accept request
-    client_size = sizeof(client_addr);
-    client_sd = accept(server_sd, (struct sockaddr*) &client_addr, &client_size);
-    if (client_sd < 0)
-        perror("accept");
-    
-    // Request logging
-    printf("Request from %s:%d\n", inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
-    
+    while (1) {
+        client_size = sizeof(client_addr);
+        client_sd = accept(server_sd, (struct sockaddr*) &client_addr, &client_size);
+        if (client_sd < 0)
+            perror("accept");
+        // Request logging
+        printf("Request from %s:%d\n", inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
 
-    char msg[] = "Hello client !\n";
-    write(client_sd, msg, sizeof(msg));
+        handle_request(client_sd);
+    }
 
     close(server_sd);
-    close(client_sd);
     
     return 0;
 }
@@ -87,14 +92,14 @@ int take_input(char **args)
 {
     int port = -1;
 
-    char buf[MAX_LINE];
-    fgets(buf, MAX_LINE, stdin);
-    buf[strlen(buf)-1] = 0; // ignore '\n' character
+    char line[MAX_LINE];
+    fgets(line, MAX_LINE, stdin);
+    line[strlen(line)-1] = 0; // ignore '\n' character
 
     // String parsing
     char *parsed;
     int i = 0;
-    parsed = strtok(buf, " ");
+    parsed = strtok(line, " ");
     while (parsed != NULL) {
         args[i++] = parsed;
         parsed = strtok(NULL, " ");
@@ -104,4 +109,120 @@ int take_input(char **args)
         port = atoi(args[1]);
 
     return port;
+}
+
+void handle_request(int client_sd)
+{
+    FILE *client_read;
+    FILE *client_write;
+    char line[REQ_LINE];
+
+    char method[10];
+    char file_name[30];
+    char content_type[15];
+
+    char msg[2048]; // message buffer
+
+    int received = recv(client_sd, msg, 2047, 0);
+
+    printf("-------Request message from Client-------\n");
+    printf("%s", msg);
+    printf("------------\n");
+
+    /* < -- parsing request line into method, filename, content-type -- > */
+    // client_read = fdopen(client_sd, "r");
+    // client_write = fdopen(dup(client_sd), "w");
+
+    fgets(line, REQ_LINE, client_read);
+    // looking for substring "HTTP/" -> checking the file type is http
+    if (strstr(line, "HTTP/") == NULL) {
+        send_error(client_write);
+        fclose(client_read);
+        fclose(client_write);
+        return;
+    }
+
+    // check requested method is GET
+    strcpy(method, strtok(line, " /"));
+    if (strcmp(method, "GET") != 0) {
+        send_error(client_write);
+        fclose(client_read);
+        fclose(client_write);
+        return;
+    }
+
+    strcpy(file_name, strtok(NULL, " /"));
+    strcpy(content_type, get_content_type(file_name));
+    fclose(client_read); // read done
+
+
+    send_data(client_write, content_type, file_name);
+
+}
+
+char *get_content_type(char *filename)
+{
+    char extension[100];
+    char file_name[100];
+
+    strcpy(file_name, filename);
+    strtok(file_name, "."); // discard filename
+    strcpy(extension, strtok(NULL, ".")); // take extension (ex .html)
+
+    if (strcmp(extension, "html") == 0 || strcmp(extension, "htm"))
+        return "text/html";
+    else {
+        return "text/plain";
+    }
+}
+
+// write data message on client
+void send_data(FILE *client_w, char *ct, char *file_name)
+{
+    char protocol[] = "HTTP/1.0 200 OK\r\n";
+    char server[] = "Server:My Own Server!\r\n";
+    char *content_type;
+    char buf[2048];
+
+    content_type = ct;
+    printf("content-type : %s\n", content_type);
+
+    // file to send
+    FILE *obj = fopen(file_name, "r");
+    int fd = fileno(client_w);
+    if (obj == NULL) {
+        printf("file not found\n");
+        send_error(client_w);
+        fclose(client_w);
+        return;
+    }
+
+    // send header to packet
+    send(fd, "HTTP/1.0 200 OK\r\n", 17, 0);
+    // fputs(protocol, client_w);
+    fputs(server, client_w);
+    fputs(content_type, client_w);
+
+    while (fgets(buf, 2048, obj) != NULL) {
+        write(fd, buf, 2048);
+        fflush(client_w);
+    }
+
+    fflush(client_w);
+    fclose(client_w);
+}
+
+// write error message on client
+void send_error(FILE *client_w)
+{
+    char protocol[] = "HTTP/1.0 400 Bad Request\r\n";
+    char server[] = "Server:My Own Server!\r\n";
+    char content_type[] = "Content-type:text/html\r\n\r\n";
+    char content[] = "<html><head><title>오류가 발생했습니다. 파일명을 확인해주세요.</title></head></html>";
+
+    // write header
+    fputs(protocol, client_w);
+    fputs(server, client_w);
+    fputs(content_type, client_w);
+    fflush(client_w);
 }
